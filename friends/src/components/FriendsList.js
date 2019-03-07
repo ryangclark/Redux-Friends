8678d9d05a7e0bead802516d31f60b6702593d70
Clark <rclark010@gmail.com>
2019-03-07 12:07:38 -0700

    Modifying FriendsList.js to work with dragging.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
-import Friend from './Friend';
+import DraggableFriend from './DraggableFriend';
+
+function reorder(list, startIndex, endIndex) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+function onDragEnd(result) {
+  // dropped outside the list
+  if (!result.destination) {
+    return;
+  }
+
+  const items = reorder(
+    this.state.items,
+    result.source.index,
+    result.destination.index
+  );
+
+  this.setState({
+    items,
+  });
+};
 
 const FriendsList = props => {
     return (
-        <section className="friends-list">
+        <section className="friends-list-container">
           <h2>Friends List</h2>
-          {
-            props.friendsList
-            ? props.friendsList.map((friend, index) =>
-                <Friend {...friend} key={friend.id} rank={index + 1} />
-              )
-            : <p className="loading">Loading Friends!</p>
-          }
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(provided, snapshop) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                >
+                  {
+                    props.friendsList
+                    ? props.friendsList.map((friend, index) =>
+                        <DraggableFriend
+                          {...friend}
+                          draggableId={friend.id}
+                          key={friend.id}
+                          index={index}
+                        />
+                      )
+                    : <p className="loading">Loading Friends!</p>
+                  }
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
         </section>
     );
 };
